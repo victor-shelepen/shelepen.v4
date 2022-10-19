2c00127d981db49ba307d3cc011f4f42ccfed9fd
Victor Shelepen <victor.shelepen@gmail.com>
2022-10-19 22:17:25 +0300

    Webpack file loader.

--- webpack.code.config.js
+++ webpack.code.config.js
@@ -16,6 +16,14 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
         test: /\.pug$/,
         loader: 'pug-loader',
         options: {
